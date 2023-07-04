81f96f114fc4db8f0f981811cd01995d521e69b0
sheringugi <sheri.ngugi17@gmail.com>
2023-07-04 08:10:57 +0300

    Update home component

--- src/components/App.js
+++ src/components/App.js
@@ -2,10 +2,12 @@ import CreateAccount from "../CreateAccount";
 import Login from "../Login";
 import "./App.css";
 import React from "react";
+import Home from "../Home";
 
 function App() {
   return (
     <>
+      <Home />
       <CreateAccount />
       <Login />
     </>
