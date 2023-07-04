416be3df46898ce73eb56d1387e87019e07480de
sheringugi <sheri.ngugi17@gmail.com>
2023-07-04 13:42:52 +0300

    Create protected routes

---
+++ src/components/Dashboard.js
@@ -0,0 +1,12 @@
+import React from "react"; 
+
+function Dashboard() {
+
+    return(
+
+        <h3>Dashboard</h3>
+
+    )
+}
+
+export default Dashboard;
---
+++ src/components/Settings.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+function Settings(){
+    return(
+
+        <h3>Settings</h3>
+    )
+
+}
+export default Settings;
