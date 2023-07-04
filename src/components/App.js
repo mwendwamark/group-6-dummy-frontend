c64abe84151c056b9df291c8cdf0ea796ebd2691
sheringugi <sheri.ngugi17@gmail.com>
2023-07-04 07:36:46 +0300

    Update Login component

--- src/components/App.js
+++ src/components/App.js
@@ -1,31 +1,13 @@
 import CreateAccount from "../CreateAccount";
+import Login from "../Login";
 import "./App.css";
 import React from "react";
 
 function App() {
   return (
     <>
-      <div className="background">
-        <div className="shape"></div>
-        <div className="shape"></div>
-      </div>
-      <form>
-        <h3>Login Here</h3>
-        <label htmlFor="username">Username</label>
-        <input type="text" placeholder="Email or Phone" id="username" />
-        <label htmlFor="password">Password</label>
-        <input type="password" placeholder="Password" id="password" />
-        <button>Log In</button>
-        <div className="social">
-          <div className="go">
-            <i className="fab fa-google"></i> Google
-          </div>
-          <div className="fb">
-            <i className="fab fa-facebook"></i> Facebook
-          </div>
-        </div>
-      </form>
       <CreateAccount />
+      <Login />
     </>
   );
 }
