fb7414d9a03315cf2f8ffb674571bc30967052b0
sheringugi <sheri.ngugi17@gmail.com>
2023-07-04 07:54:16 +0300

    Update home component

--- src/Home.js
+++ src/Home.js
@@ -2,6 +2,15 @@ import React from "react";
 
 function Home(){
 
+    return(
+
+        <div>
+            <h3>
+                Home
+            </h3>
+        </div>
+    )
+
 }
 
 export default Home;
